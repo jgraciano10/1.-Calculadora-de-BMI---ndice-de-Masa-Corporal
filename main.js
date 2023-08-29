c23537d700cbd8e683abffea86974c02f4b49355
jgraciano10 <graciano.inluencia@gmail.com>
2023-08-29 12:00:15 -0500

    interective

--- main.js
+++ main.js
@@ -3,17 +3,26 @@ function indice(peso, altura){
     let IBM = peso / altura**2
     console.log(IBM)
     if (IBM<18.5){
-        console.log("bajo peso")
+        console.log("Su peso se considera bajo en proporcion con su altura")
 
     }else if (IBM>=18.5 && IBM<=24.9){
-        console.log("normal")
+        console.log("Su peso se considera normal en proporcion con su altura")
 
     }else if (IBM>=25 && IBM<=29.9){
-        console.log("sobrepeso")
+        console.log("Su peso se considera en sobrepeso en proporcion con su altura")
 
     }else{
-        console.log("obesidad")
+        console.log("obesidadSu peso se considera en obesidad en proporcion con su altura")
     }
 }
+
+function saveInput() {
+    const userPeso = document.getElementById('userPeso').value;
+    const userAltura = document.getElementById('userAltura').value;
+    // You can now use 'userInput' to store or process the input further.
+    indice(userPeso,userAltura)
+    
+  }
+
 
 
